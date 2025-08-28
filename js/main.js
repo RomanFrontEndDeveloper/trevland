8e29992b41c1773bb0d8e258395f83c7ffef09c4
RomanFronrEnd <romariotraveler@gmail.com>
2025-08-28 11:38:42 +0300

    mobil adaptive section slider

--- js/main.js
+++ js/main.js
@@ -9,6 +9,50 @@ $(document).ready(function () {
 			'<button type="button" class="btn-slick-prev btn btn-slider"><img src="images/prev.svg" alt="" /></button>',
 		nextArrow:
 			'<button type="button" class="btn-slick-next btn btn-slider"><img src="images/next.svg" alt="" /></button>',
+		responsive: [
+			{
+				breakpoint: 1401,
+				settings: {
+					infinite: true,
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 1101,
+				settings: {
+					infinite: true,
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 850,
+				settings: {
+					infinite: true,
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					dots: true,
+					arrows: false,
+					autoplay: true,
+					autoplaySpeed: 2000,
+				},
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					infinite: true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: true,
+					arrows: false,
+					autoplay: true,
+					autoplaySpeed: 2000,
+				},
+			},
+		],
 	});
 	$('.mibile-menu').click(function () {
 		$('.mibile-menu span').toggleClass('active');
